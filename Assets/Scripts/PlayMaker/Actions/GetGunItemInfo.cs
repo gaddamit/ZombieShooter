4df5c9e16001912dcfe186ed62d94ab05b9a443c
Leo Esguerra <leo.esguerra@gmail.com>
2024-10-17 10:35:16 -0600

    FSM action to load SO data to prefab

---
+++ Assets/Scripts/PlayMaker/Actions/GetGunItemInfo.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+
+	[ActionCategory("CustomActions")]
+	public class GetGunItemInfo : FsmStateAction
+	{
+		public FsmString gunName;
+		public FsmString description;
+		public FsmFloat fireRate;
+		public FsmFloat range;
+		public FsmInt maxAmmo;
+		public FsmFloat recoil;
+		public FsmBool isSpread;
+		public FsmFloat bulletSpeed;
+		public FsmFloat bulletLifeTime;
+		[ObjectType(typeof(GameObject))]
+		public FsmObject bulletPrefab;
+		[ObjectType(typeof(Sprite))]
+		public FsmObject gunSprite;
+		[ObjectType(typeof(GunSO))]
+		public FsmObject ItemReference;
+		// Code that runs on entering the state.
+		public override void OnEnter()
+		{
+			gunName.Value = ((GunSO)ItemReference.Value).gunName;
+			description.Value = ((GunSO)ItemReference.Value).description;
+			fireRate.Value = ((GunSO)ItemReference.Value).fireRate;
+			range.Value = ((GunSO)ItemReference.Value).range;
+			maxAmmo.Value = ((GunSO)ItemReference.Value).maxAmmo;
+			recoil.Value = ((GunSO)ItemReference.Value).recoil;
+			isSpread.Value = ((GunSO)ItemReference.Value).spread;
+			bulletSpeed.Value = ((GunSO)ItemReference.Value).bulletSpeed;
+			bulletLifeTime.Value = ((GunSO)ItemReference.Value).bulletLifeTime;
+			bulletPrefab.Value = ((GunSO)ItemReference.Value).bulletPrefab;
+			gunSprite.Value = ((GunSO)ItemReference.Value).gunSprite;
+			
+			Finish();
+		}
+
+
+	}
+
+}
