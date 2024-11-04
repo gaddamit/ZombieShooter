105a4f4039952426978b2daa5a03814bb5ebfe3a
rougecode <koenigrojass@gmail.com>
2024-11-04 16:15:38 -0700

    CapturePoint works with tileset
    Still have ti make it so the doors open and i think the way to do it is to switch the tree door sprites

--- Assets/Art/Sprites/Tileset/button_press.cs
+++ Assets/Art/Sprites/Tileset/button_press.cs
@@ -1,16 +1,26 @@
+using System;
 using HutongGames.PlayMaker.Actions;
 using System.Collections;
 using System.Collections.Generic;
+using HutongGames.PlayMaker;
 using UnityEngine;
 
 public class button_press : MonoBehaviour
 {
     private Animator animator;
+    private PlayMakerFSM CapturepointFSM;
     private void Start()
     {
         animator = GetComponent<Animator>();
         animator.Play("inactive");
+        CapturepointFSM = GetComponent<PlayMakerFSM>();
     }
+
+    private void Update()
+    {
+        CheckCapturepointTimer();
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
@@ -26,6 +36,18 @@ public class button_press : MonoBehaviour
         {
             animator.Play("inactive");
             Debug.Log("exit");
+        }
+    }
+
+    private void CheckCapturepointTimer()
+    {
+        FsmFloat timer = CapturepointFSM.FsmVariables.GetFsmFloat("CaptureTimer");
+        
+        if (timer.Value <= 0)
+        {
+            animator.Play("activated");
+            animator.speed = 0;
+            animator.Play("activated", 0, 0);
         }
     }
 }
